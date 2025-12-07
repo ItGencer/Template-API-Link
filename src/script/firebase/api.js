556840a39dfa8fee6f9da6563918a5b4267e9f8c
ItGencer <gencer.it.1989@gmail.com>
2025-12-07 18:29:40 +0200

    add api conect

--- src/script/firebase/api.js
+++ src/script/firebase/api.js
@@ -1,17 +1,48 @@
 const apiLinkFirebase = "https://template-api-link-default-rtdb.europe-west1.firebasedatabase.app/.json";
 
-function getApiData(link) {
-  return new Promise((resolve, reject) => {
-    fetch(link)
-      .then(data => resolve(data.json()))
-      .catch(err => reject(err));
-  });
-}
+class ApiClass {
+  #apiLink;
+  
+  constructor(apiLink){
+    this.#apiLink = apiLink;
+  }
 
-getApiData(apiLink)
-  .then(data => {
-    console.log("Отримані дані:", data);
-  })
-  .catch(error => {
-    console.error("Помилка:", error);
-  });
+  get getApiData(){
+    this.getDataFunct(this.#apiLink);
+  }
+  
+  set setApiData(data){
+    this.setDataFunct(this.#apiLink, data);
+  }
+
+  getDataFunct(link){
+    const apiDataPromise = fetch(link);
+    return apiDataPromise
+      .then(data =>{
+        if (!data.ok) {
+          throw new Error("Data is not ok");
+        }
+        return data.json();
+      })
+      .then(data => {
+        try {
+          data.forEach(element => {
+            console.log(element);
+          });
+        }
+        catch (error) {
+          console.error(`Data is not - ${error}`);
+        }
+      })
+      .catch(error => console.error(error));
+  }
+
+  setDataFunct(link, data){
+    fetch(link, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify([data])
+    })
+    .then(result => result .json());
+  }
+}
